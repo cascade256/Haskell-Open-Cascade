45a48dbdf6610ef04c1c55357aaede8604160221
cascade256 <128hi256@gmail.com>
2016-02-15 18:59:15 -0800

    Continue working on Geom2D

--- src/Geom2D.hs
+++ src/Geom2D.hs
@@ -1,19 +1,25 @@
 module Geom2D where
   type StandardReal = Double
-
-  data Matrix = Matrix
-
-  data Point = Point StandardReal StandardReal
-
+  type Vector = Point
   type Axis = Point
 
-  data Transform = Transform StandardReal Matrix Point
+  data Matrix = Matrix [[StandardReal]]
+  identity = Matrix [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+  at :: Matrix -> Int -> Int -> StandardReal
+  at (Matrix mat) x y = (mat !! y) !! x
+
+  createTranslation :: Point -> Transform
+  createTranslation = Transform Translation 1.0 identity
+
+  data TransformType = Identity | Translation | Scale | Mirror
+  data Transform = Transform TransformType StandardReal Matrix Point
+  matrix (Transform _ _ m _) = m
 
   class Transformable a where
     transform :: a -> Transform -> a
-
   class Curve a where
     value :: a -> StandardReal -> Point
+    dN :: a -> StandardReal -> StandardReal -> (Point, [Point])
 
   data Conic = Circle Point StandardReal
              | Ellipse Point Point StandardReal
@@ -24,13 +30,24 @@ module Geom2D where
                     | BSplineCurve
                     | TrimmedCurve
 
+  data Point = Point StandardReal StandardReal
+    deriving(Show)
+  x (Point x _) = x
+  y (Point y _) = y
+  instance Transformable Point where
+    transform p (Transform Identity _ _ _) = p
+    transform p (Transform Translation _ _ l) = Point (x p + x l) (y p + y l)
+    transform p (Transform _ _ mat _) = Point newX newY where
+      oldX = x p
+      oldY = y p
+      newX = at mat 0 0 * oldX + at mat 0 1 * oldY
+      newY = at mat 1 0 * oldX + at mat 1 1 * oldY
 
-  --data Curve = Line Point Point
-    --         | Conic
-      --       | BoundedCurve
-        --     | OffsetCurve
-
-  data Vector = Direction
-              | VectorWithMagnitude
-
-  data Geometry = Geometry Transformable
+  data Line = Line Point Point
+  lineDir (Line _ dir) = dir
+  instance Curve Line where
+    value (Line (Point posX posY) (Point dirX dirY)) u = Point (u * dirX + posX) (u * dirY + posY)
+    dN l u 1 = (value l u, [lineDir l])
+    dN l u n = (value l u, Point 0 0 : snd (dN l u (n - 1)))
+  instance Transformable Line where
+    transform (Line pos dir) t = Line (transform pos t) dir
